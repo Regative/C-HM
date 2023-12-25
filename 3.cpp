ca0c4c4024dedbb1a060df60487faed74f93c0d5
Regative <154831396+Regative@users.noreply.github.com>
2023-12-25 21:21:41 +0300

    Add files via upload

---
+++ 3.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+void swapEvenOdd(int* arr, int size);
+
+int main() {
+    const int size = 12;
+    int* dynamicArray = new int[size];
+
+    for (int i = 0; i < size; ++i) {
+        dynamicArray[i] = i + 1;
+    }
+
+    std::cout << "Original array: ";
+    for (int i = 0; i < size; ++i) {
+        std::cout << dynamicArray[i] << " ";
+    }
+    std::cout << std::endl;
+
+    swapEvenOdd(dynamicArray, size);
+
+    std::cout << "Array after swapping even and odd elements: ";
+    for (int i = 0; i < size; ++i) {
+        std::cout << dynamicArray[i] << " ";
+    }
+    std::cout << std::endl;
+
+    delete[] dynamicArray;
+    return 0;
+}
+
+void swapEvenOdd(int* arr, int size) {
+    for (int i = 0; i < size - 1; i += 2) {
+        int temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+    }
+}
